d04afe78442464e48ba212b1f5a5e3a8c618502b
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-09 11:53:41 -0800

    completed all problems prior to depthFirstSearch. depthFirstSearch needs to be checked if correct.

--- graph.js
+++ graph.js
@@ -16,26 +16,67 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addNode(node) { }
+  addNode(node) {
+    this.nodes.add(node);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addNodes(nodeArray) { }
+  addNodes(nodeArray) {
+    for (let node of nodeArray) {
+      this.addNode(node);
+    }
+  }
 
   /** add edge between nodes n1,n2 */
-  addEdge(n1, n2) { }
+  addEdge(n1, n2) {
+    n1.adjacent.add(n2);
+    n2.adjacent.add(n1);
+  }
 
   /** remove edge between nodes n1,n2 */
-  removeEdge(n1, n2) { }
+  removeEdge(n1, n2) {
+    n1.adjacent.delete(n2);
+    n2.adjacent.delete(n1);
+  }
 
   /** remove node from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that node
    */
-  removeNode(node) { }
+  removeNode(node) {
+    for (let neighbor of node.adjacent) {
+      neighbor.adjacent.delete(node);
+    }
+
+    this.nodes.delete(node);
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start) {
+    //in order -> recursion, push vertex, recursion
+
+    let results = [];
+    let stack = [start];
+    let seen = new Set();
+
+    while (stack.length) {
+      let current = stack.pop();
+
+      if (!seen.has(current)) {
+        seen.add(current);
+        results.push(current);
+
+        for (let neighbor of current.adjacent) {
+          if (!seen.has(neighbor)) {
+            stack.push(neighbor);
+          }
+        }
+      }
+    }
+
+    return results;
+  }
 
   /** traverse graph with BDS and returns array of Node values */
   breadthFirstSearch(start) { }
@@ -44,4 +85,4 @@ class Graph {
   distanceOfShortestPath(start, end) { }
 }
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node };
